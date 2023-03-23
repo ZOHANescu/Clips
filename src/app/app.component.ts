b640aa005167bf775e04590cba6818d2c952fac3
StefanH <stefanhera@gmail.com>
2023-03-23 15:50:47 +0200

    Memory leak issue presented and created.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { ModalService } from './services/modal.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'clips';
+    showModal = true;
+
+    constructor(public modal: ModalService) {
+        this.ngOnInit();
+    }
+
+    ngOnInit() {
+        setInterval(
+            () => {
+                this.showModal = !this.showModal,
+                console.log(this.modal.modals);
+            },
+            1000
+        );
+    }
 }
--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -1,21 +1,38 @@
 import { Injectable } from '@angular/core';
 
+interface IModal {
+    id: string;
+    visible: boolean;
+}
+
 @Injectable({
     providedIn: 'root'
 })
 export class ModalService {
 
-    private visible = false;
+    public modals: IModal[] = [];
 
     constructor() {  
 
     }
 
-    public isModalVisible() {
-        return this.visible;
+    public register(id: string) {
+        this.modals.push({
+            id,
+            visible: false
+        });
     }
 
-    public toggleModal(): void {
-        this.visible = !this.visible;
+    public isModalVisible(id: string) : boolean {
+        return !!this.modals.find(element => element.id === id)?.visible;
+    }
+
+    public toggleModal(id: string) : void {
+
+        const modal = this.modals.find(element => element.id === id);
+
+        if(modal) {
+            modal.visible = !modal.visible;
+        }
     }
 }
