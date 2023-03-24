54161f20e352cf685d1022b3073b4ad4740bf683
StefanH <stefanhera@gmail.com>
2023-03-24 14:17:58 +0200

    Added feature to switch between Register and Login tab

--- src/app/shared/tab/tab.component.ts
+++ src/app/shared/tab/tab.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tab',
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./tab.component.css']
 })
 export class TabComponent {
+
+  @Input() tabTitle = '';
+  @Input() active = false;
+
+  constructor() {}
+
+  // public getTabTitle() : string {
+  //   return this.tabTitle;
+  // }
+
+  // public setTabTitle(title: string) : void {
+  //   this.tabTitle = title;
+  // }
 
 }
--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component, ContentChildren, QueryList } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
 
 @Component({
-  selector: 'app-tabs-container',
-  templateUrl: './tabs-container.component.html',
-  styleUrls: ['./tabs-container.component.css']
+    selector: 'app-tabs-container',
+    templateUrl: './tabs-container.component.html',
+    styleUrls: ['./tabs-container.component.css']
 })
-export class TabsContainerComponent {
 
+export class TabsContainerComponent implements AfterContentInit{
+
+    // @ContentChildren(TabComponent) tabs = {};
+
+    // Annotated the tabs array for type-safety and auto-completion suggestions (not mandatory but better coding exp.)
+    @ContentChildren(TabComponent) tabs: QueryList<TabComponent> = new QueryList();
+    // This can work too
+    // @ContentChildren(TabComponent) tabs?: QueryList<TabComponent>;
+
+    constructor() {}
+
+    ngAfterContentInit(): void {
+        const activeTabs = this.tabs?.filter(
+            tab => tab.active
+        );
+
+        if(!activeTabs || activeTabs.length === 0) {
+            this.selectTab(this.tabs!.first);
+        }
+    }
+
+    selectTab(tab: TabComponent){
+        this.tabs?.forEach(tab => {
+            tab.active = false;
+        });
+
+        tab.active = true;
+
+        return false;
+    }
 }
