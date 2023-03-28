ef73ec673e218a7bfdef543a8fbf0346252f85ec
StefanH <stefanhera@gmail.com>
2023-03-28 18:15:06 +0300

    Added validation on every field in the form. Validation not complete on the Confirm Password field.

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthModalComponent } from './auth-modal/auth-modal.component';
 import { SharedModule } from '../shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AuthModalComponent } from './auth-modal/auth-modal.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AuthModalComponent
+    AuthModalComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ],
   exports: [
-    AuthModalComponent
+    AuthModalComponent,
   ]
 })
 export class UserModule { }
